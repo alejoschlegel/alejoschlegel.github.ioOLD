40d7d24d7dd34fcdcbee44041ff96b56da50a15a
schale29 <alejofschlegel@gmail.com>
2022-04-19 21:08:03 -0300

    Updated project cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,60 +2,59 @@ export default function Home () {
   return (
     <div className="home">
       <div className="home_header">
-        <h1>ALEJO SCHLEGEL</h1>
-        <h2>JUNIOR SOFTWARE ENGINEER</h2>
+        <h3 className="text_muted">ALEJO SCHLEGEL</h3>
+        <h1>JUNIOR SOFTWARE ENGINEER</h1>
 
-        <h4>SEE MY PROJECTS!!</h4>
+        <a href="" className="text_muted"> 
+          <h3>SEE ALL MY PROJECTS</h3>
+        </a>
       </div>
 
       <div className="projects">
 
         <div className="project">
-          <div className="project_header">
-            <h3>Project 1</h3>
-            <h2>A monetizatio platform that helps publishers</h2>
+          <div className="project_info">
+            <h2>Moon protocol</h2>
+            <h4>A monetizatio platform that helps publishers</h4>
+            <div className="project_tags">
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+            </div>
           </div>
-          <div className="project_tags">
-            <div className="tag"> HTML5 </div>
-            <div className="tag"> CSS3 </div>
-            <div className="tag"> JavaScript </div>
-            <div className="tag"> React </div>
-            <div className="tag"> Node.js </div>
-            <div className="tag"> Express </div>
-            <div className="tag"> MongoDB </div>
-          </div>
+          <img src="/moon_protocol.svg" />
         </div>
 
         <div className="project">
-          <div className="project_header">
-            <h3>Project 1</h3>
-            <h2>A monetizatio platform that helps publishers</h2>
+          <div className="project_info">
+            <h2>Project 1</h2>
+            <h4>A monetizatio platform that helps publishers</h4>
+            <div className="project_tags">
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+            </div>
           </div>
-          <div className="project_tags">
-            <div className="tag"> HTML5 </div>
-            <div className="tag"> CSS3 </div>
-            <div className="tag"> JavaScript </div>
-            <div className="tag"> React </div>
-            <div className="tag"> Node.js </div>
-            <div className="tag"> Express </div>
-            <div className="tag"> MongoDB </div>
-          </div>
+          <img src="/moon_protocol.svg" />
         </div>
 
         <div className="project">
-          <div className="project_header">
-            <h3>Project 1</h3>
-            <h2>A monetizatio platform that helps publishers</h2>
+          <div className="project_info">
+            <h2>Project 1</h2>
+            <h4>A monetizatio platform that helps publishers</h4>
+            <div className="project_tags">
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+              <div className="tag">HTML</div>
+            </div>
           </div>
-          <div className="project_tags">
-            <div className="tag"> HTML5 </div>
-            <div className="tag"> CSS3 </div>
-            <div className="tag"> JavaScript </div>
-            <div className="tag"> React </div>
-            <div className="tag"> Node.js </div>
-            <div className="tag"> Express </div>
-            <div className="tag"> MongoDB </div>
-          </div>
+          <img src="/moon_protocol.svg" />
         </div>
 
       </div>
