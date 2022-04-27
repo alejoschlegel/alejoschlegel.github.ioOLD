b39922ea8399066d5462b24fd5680952724a7beb
schale29 <alejofschlegel@gmail.com>
2022-04-27 01:00:20 -0300

    Updated projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,84 +4,43 @@ export default function Home () {
       <div className="home_header">
         <h3 className="text_muted">ALEJO SCHLEGEL</h3>
         <h1>JUNIOR SOFTWARE ENGINEER</h1>
+      </div>
 
-        <a href="" className="text_muted"> 
-          <h3>SEE ALL MY PROJECTS</h3>
-        </a>
+      <div className="home_body">
+      <h2 className="text_muted">SEE MY LASTEST PROJECTS</h2>
       </div>
 
       <div className="projects_test">
 
-        <div className="project_card_test moon_protocol">
-          <div>
-            <h4>Moon protocol</h4>
-            <p> A social media that allows users to create, search and buy art. </p>
-          </div>
-        </div>
-
-        <div className="project_card_test moon_protocol">
-          <div>
-            <h4>Moon protocol</h4>
-            <p> A social media that allows users to create, search and buy art. </p>
-          </div>
-        </div>
-
-        <div className="project_card_test moon_protocol">
-          <div>
-            <h4>Moon protocol</h4>
-            <p> A social media that allows users to create, search and buy art. </p>
-          </div>
+      <div className="project_card_large admin_dashboard">
+        <div className="project_info_large">
+          <h4>Administrative dashboard</h4>
+          <p>An app that provides administrators direct access to important Encompass tools for faster and more convenient community management. The Dashboard serves as an administrative home page with access to key components of your community.</p>
         </div>
       </div>
 
-      {/* <div className="projects">
-
-        <div className="project">
-          <div className="project_info">
-            <h2>Moon protocol</h2>
-            <h4>A monetizatio platform that helps publishers</h4>
-            <div className="project_tags">
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-            </div>
-          </div>
-          <img src="/moon_protocol.svg" />
+      <div className="project_card moon_protocool">
+        <div className="project_info">
+          <h4>Moon protocol</h4>
+          <p> A social media that allows users to create, search and buy art. With its corresponding database, payment system, real-time notifications and administrator panel </p>
         </div>
+      </div>
 
-        <div className="project">
-          <div className="project_info">
-            <h2>Weather app</h2>
-            <h4>A monetizatio platform that helps publishers</h4>
-            <div className="project_tags">
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-            </div>
-          </div>
-          <img src="/moon_protocol.svg" />
+      <div className="project_card todo_app">
+        <div className="project_info">
+          <h4>To Do App</h4>
+          <p>App to keep our day-to-day tasks or list everything we have to do, with the most important tasks at the top of the list and the less important tasks at the bottom. It is helpful in planning our daily schedules..</p>
         </div>
+      </div>
 
-        <div className="project">
-          <div className="project_info">
-            <h2>Admin panel</h2>
-            <h4>A monetizatio platform that helps publishers</h4>
-            <div className="project_tags">
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-              <div className="tag">HTML</div>
-            </div>
-          </div>
-          <img src="/moon_protocol.svg" />
+      <div className="project_card_large weather_app">
+        <div className="project_info_large">
+          <h4>Weather App</h4>
+          <p> Easy and effective way to instantly access forecasts, warnings and other useful weather information. It gives the public unprecedented flexibility: users choose the information they want to see. </p>
         </div>
+      </div>
 
-      </div> */}
+      </div>
     </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav () {
                     <a><span class="material-icons-sharp">menu</span></a>
                     <ul className="vertical_menu">
                         <li><a href="#"><span class="material-icons-sharp" title="Home">home</span></a></li>
-                        <li><a href="#"><span class="material-icons-sharp" title="About me">person</span></a></li>
+                        {/* <li><a href="#"><span class="material-icons-sharp" title="About me">person</span></a></li> */}
                         <li><a href="/Alejo_Schlegel_resume.pdf" target="_blank" title="Resume"><span class="material-icons-sharp">description</span></a></li>
                     </ul>
                 </li>
